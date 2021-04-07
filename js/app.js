b7358c05b2e8d00a95bb86e72a636653a9a9ce01
BernardoG <A00570682@itesm.mx>
2021-04-07 14:50:29 -0500

    All done

--- js/app.js
+++ js/app.js
@@ -3,37 +3,37 @@
 //Laboratory #4: To Do List, 1st practice of Javascript basic functions
 
 //Declare variables for buttons and managing areas
-var btnPost = document.querySelector('.submitButton');
-var btnClear = document.querySelector('.clearButton');
-var btnMark = document.querySelector('.markAllButton');
-var btnDelete = document.querySelector('.deleteButton');
+var btnPost = $('.submitButton');
+var btnClear = $('.clearButton');
+var btnMark = $('.markAllButton');
+var btnDelete = $('.deleteButton');
 
-var todoAreaText = document.querySelector('#todoText');
-var todoList = document.querySelector('#listTodos');
+var todoAreaText = $('#todoText');
+var todoList = $('#listTodos');
 var allTodos;
 var todoText;
 var i = 0;
 
 
 //Events on buttons
-btnPost.addEventListener("click", postTodo);
-btnClear.addEventListener("click", clearTodo);
-btnMark.addEventListener("click", markTodo);
-btnDelete.addEventListener("click", deleteTodo);
+btnPost.on("click", postTodo);
+btnClear.on("click", clearTodo);
+btnMark.on("click", markTodo);
+btnDelete.on("click", deleteTodo);
 
 
 //Functions
 
 function postTodo(e){
     e.preventDefault();
-    todoText = todoAreaText.value;
-    todoList.innerHTML = todoList.innerHTML + '<p> <input type="checkbox" class="todos"> ' + todoText + "</p>" ;  
-    todoAreaText.value = null;
+    todoText = todoAreaText.val();
+    todoList.html(todoList.html() + '<p> <input type="checkbox" class="todos"> ' + todoText + "</p>") ;  
+    todoAreaText.val(null);
 }
 
 function clearTodo(e){
     e.preventDefault();
-    allTodos = document.querySelectorAll('.todos');    
+    allTodos = $('.todos');    
     for (let i = 0; i < allTodos.length; i++) {
         allTodos[i].checked = false;        
     }    
@@ -41,7 +41,7 @@ function clearTodo(e){
 
 function markTodo(e){
     e.preventDefault();
-    allTodos = document.querySelectorAll('.todos');    
+    allTodos = $('.todos');    
     for (let i = 0; i < allTodos.length; i++) {
         allTodos[i].checked = true;        
     }    
@@ -49,7 +49,7 @@ function markTodo(e){
 
 function deleteTodo(e) {
     e.preventDefault();
-    todoList.innerHTML = null;
+    todoList.html(null);
 }
 
     
